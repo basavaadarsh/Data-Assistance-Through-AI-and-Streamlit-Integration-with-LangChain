06b5b7443228cb78c22383375677cb7db797c433
basavaadarsh <basavaadarsh4@gmail.com>
2024-08-07 23:55:29 +0530

    added html in streamlit for phi3.py code

--- phi3.py
+++ phi3.py
@@ -21,3 +21,22 @@ def steps_eda():
         return "Error retrieving steps of EDA"
 
 # Continue with your existing code
+# Title
+st.title('AI Assistant for Data Science 🤖')
+
+# Welcoming message
+st.write("Hello, 👋 I am your AI Assistant and I am here to help you with your data science projects.")
+
+# Explanation sidebar
+with st.sidebar:
+    st.write('*Your Data Science Adventure Begins with a CSV File.*')
+    st.caption('''**You may already know that every exciting data science journey starts with a dataset.
+    That's why I'd love for you to upload a CSV file.
+    Once we have your data in hand, we'll dive into understanding it and have some fun exploring it.
+    Then, we'll work together to shape your business challenge into a data science framework.
+    I'll introduce you to the coolest machine learning models, and we'll use them to tackle your problem. Sounds fun right?**
+    ''')
+
+    st.divider()
+
+    st.caption("<p style ='text-align:center'> made with ❤️ by Ana</p>", unsafe_allow_html=True)
